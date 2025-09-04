ba2492fd0d379a493a5b31255fe6752194c56986
supriyamugeraya <mugerayasupriya@gmail.com>
2025-09-04 19:15:14 +0530

    First Commit

---
+++ DotNetDemo.API/Controllers/WeatherForecastController.cs
@@ -0,0 +1,38 @@
+using System.Diagnostics;
+using DotNetDemo.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetDemo.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : Controller
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing","Bracing","Chilly","Cool","Mild","Warm","Balmy","Hot","Sweltering","Scorching"
+        };
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        
+        [HttpGet(Name = "GetWeatherForecast")]
+
+        public IEnumerable<WeatherForecast> Get()
+        {   
+            return Enumerable.Range(1,5).Select(index=> new WeatherForecast
+            {
+                Date=DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC=Random.Shared.Next(-20,55),
+                Summary =Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+                .ToArray();
+
+        }
+
+    }
+}
---
+++ DotNetDemo.API/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DotNetDemo.API.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
