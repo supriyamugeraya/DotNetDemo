162a77f545fe9b0842bf9a5a90f60b7ca69464fa
supriyamugeraya <mugerayasupriya@gmail.com>
2025-09-04 21:49:34 +0530

    Created RestController fro students to fetch details

---
+++ DotNetDemo.API/Controllers/StudentsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetDemo.API.Controllers
+{
+
+    //https://localhost:portnumber(7032)/api/students
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        // GET: https://localhost:portnumber(7032)/api/student
+        [HttpGet]
+        public IActionResult GetAllStudents()
+
+        {
+            string[] StudentName = new string[] { "supriya", "sumana", "varun", "shreyaan", "manasvi" };
+            return Ok(StudentName);
+        }
+    }
+}
--- DotNetDemo.API/obj/Debug/net8.0/DotNetDemo.API.AssemblyInfo.cs
+++ DotNetDemo.API/obj/Debug/net8.0/DotNetDemo.API.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotNetDemo.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba2492fd0d379a493a5b31255fe6752194c56986")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotNetDemo.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotNetDemo.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
