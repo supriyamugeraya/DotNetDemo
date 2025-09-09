bcaaffc0834c0695e11f68db001acf6a93fc7a8a
supriyamugeraya <mugerayasupriya@gmail.com>
2025-09-09 09:59:06 +0530

    Seeding Roles in WalksAuthDbContext.

--- DotNetDemo.API/DbData/WalksAuthDbContext.cs
+++ DotNetDemo.API/DbData/WalksAuthDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace DotNetDemo.API.DbData
@@ -9,5 +10,30 @@ namespace DotNetDemo.API.DbData
         {
         }
 
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            var ReaderRoleId = "42c277cf-38d9-4704-9ca9-566efe93ccb3";
+            var WriterRoleId = "56b8ce6c-fd9a-4f8d-8071-a69b58aba32d";
+            var roles = new List<IdentityRole>
+            {
+                new IdentityRole
+                {
+                    Id = ReaderRoleId,
+                    ConcurrencyStamp = ReaderRoleId,
+                    Name = "Reader",
+                    NormalizedName = "Reader".ToUpper()
+                },
+                new IdentityRole
+                {
+                    Id = WriterRoleId,
+                    ConcurrencyStamp = WriterRoleId,
+                    Name = "Writer",
+                    NormalizedName = "Writer".ToUpper()
+                }
+            };
+
+            builder.Entity<IdentityRole>().HasData(roles);
+        }
     }
 }
