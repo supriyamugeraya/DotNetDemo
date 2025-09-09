819791ad199bfa4e79e99f9f853b91e57ed8b263
supriyamugeraya <mugerayasupriya@gmail.com>
2025-09-09 09:22:00 +0530

    Corrected missmatch Package.

--- DotNetDemo.API/obj/Debug/net8.0/DotNetDemo.API.AssemblyInfo.cs
+++ DotNetDemo.API/obj/Debug/net8.0/DotNetDemo.API.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotNetDemo.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c78964861a33538d37a819f5a6f4834994419f42")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f61311ce03869a0056eac55bcaec0eaaaaaa55a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotNetDemo.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotNetDemo.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
