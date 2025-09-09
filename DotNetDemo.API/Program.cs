7ec378db3aebd064cce0c6f803e625c0d5ddb162
supriyamugeraya <mugerayasupriya@gmail.com>
2025-09-09 16:10:31 +0530

    Added Authorization To Swagger.

--- DotNetDemo.API/Program.cs
+++ DotNetDemo.API/Program.cs
@@ -6,88 +6,122 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddDbContext<WalksDbContext>(options =>
-options.UseSqlServer(builder.Configuration.GetConnectionString("DotNetDemoConnectionString")));
-builder.Services.AddDbContext<WalksAuthDbContext>(options =>
-options.UseSqlServer(builder.Configuration.GetConnectionString("WalksAuthConnectionString")));
-
-builder.Services.AddScoped<IRegionRepository,SQLRegionRepository>();
-builder.Services.AddScoped<IWalkRepository,SQLWalkRepository>();
-
-builder.Services.AddScoped<ITokenRepository,TokenRepository>();
-
-builder.Services.AddAutoMapper(typeof(AutoMapperProfiles));
-
-builder.Services.AddIdentityCore<IdentityUser>()
-    .AddRoles<IdentityRole>()
-    .AddTokenProvider<DataProtectorTokenProvider<IdentityUser>>("Walks")
-    .AddEntityFrameworkStores<WalksAuthDbContext>()
-    .AddDefaultTokenProviders();
-
-builder.Services.Configure<IdentityOptions>(options =>
+using Microsoft.OpenApi.Models;
+internal class Program
 {
-    options.Password.RequireDigit = false;
-    options.Password.RequireLowercase = false;
-    options.Password.RequireUppercase = false;
-    options.Password.RequireNonAlphanumeric=false;
-    options.Password.RequiredLength = 6;
-    options.Password.RequiredUniqueChars = 1;
-
-});
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
-    options.TokenValidationParameters = new TokenValidationParameters
+    private static void Main(string[] args)
     {
-        ValidateIssuer = true,
-        ValidateAudience = true,
-        ValidateLifetime = true,
-        ValidateIssuerSigningKey = true,
-        ValidIssuer = builder.Configuration["Jwt:Issuer"],
-        ValidAudience = builder.Configuration["Jwt:Audience"],
-        IssuerSigningKey = new SymmetricSecurityKey(
-            Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
-    });
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddControllersWithViews();
+
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen(options =>
+        {
+            options.SwaggerDoc("v1", new OpenApiInfo { Title = "Walks API", Version = "v1" });
+            options.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme, new OpenApiSecurityScheme
+            {
+                Name = "Authorization",
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.ApiKey,
+                Scheme = JwtBearerDefaults.AuthenticationScheme
+            });
+            options.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference=new OpenApiReference
+                        {
+                            Type=ReferenceType.SecurityScheme,
+                            Id=JwtBearerDefaults.AuthenticationScheme
+                        },
+                        Scheme="Oauth2",
+                        Name=JwtBearerDefaults.AuthenticationScheme,
+                        In=ParameterLocation.Header
+                    },
+                    new List<string>()
+                }
+            });
+        });
+
+        builder.Services.AddDbContext<WalksDbContext>(options =>
+        options.UseSqlServer(builder.Configuration.GetConnectionString("DotNetDemoConnectionString")));
+        builder.Services.AddDbContext<WalksAuthDbContext>(options =>
+        options.UseSqlServer(builder.Configuration.GetConnectionString("WalksAuthConnectionString")));
+
+        builder.Services.AddScoped<IRegionRepository, SQLRegionRepository>();
+        builder.Services.AddScoped<IWalkRepository, SQLWalkRepository>();
+
+        builder.Services.AddScoped<ITokenRepository, TokenRepository>();
+
+        builder.Services.AddAutoMapper(typeof(AutoMapperProfiles));
+
+        builder.Services.AddIdentityCore<IdentityUser>()
+            .AddRoles<IdentityRole>()
+            .AddTokenProvider<DataProtectorTokenProvider<IdentityUser>>("Walks")
+            .AddEntityFrameworkStores<WalksAuthDbContext>()
+            .AddDefaultTokenProviders();
+
+        builder.Services.Configure<IdentityOptions>(options =>
+        {
+            options.Password.RequireDigit = false;
+            options.Password.RequireLowercase = false;
+            options.Password.RequireUppercase = false;
+            options.Password.RequireNonAlphanumeric = false;
+            options.Password.RequiredLength = 6;
+            options.Password.RequiredUniqueChars = 1;
+
+        });
+        builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                ValidAudience = builder.Configuration["Jwt:Audience"],
+                IssuerSigningKey = new SymmetricSecurityKey(
+                    Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+            });
 
 
 
 
-var app = builder.Build();
+        var app = builder.Build();
 
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-    
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Home/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+
+        }
+        else
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
+
+        app.Run();
+    }
 }
-else
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-
-}
-
-    app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
