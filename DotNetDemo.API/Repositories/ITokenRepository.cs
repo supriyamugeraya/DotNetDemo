b0247d143a9dd0a1f333c4b588fad0eae08681ea
supriyamugeraya <mugerayasupriya@gmail.com>
2025-09-09 13:08:09 +0530

    Created JWT token in ASP.NET core web API.

---
+++ DotNetDemo.API/Repositories/ITokenRepository.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace DotNetDemo.API.Repositories
+{
+    public interface ITokenRepository
+    {
+       string CreateJWTToken(IdentityUser user,List<string>roles );
+    }
+}
